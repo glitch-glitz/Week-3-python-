a922f7056764e6378573d363231399b265d7af12
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-05-21 02:36:26 +0300

    all.append

---
+++ relationship.py
@@ -0,0 +1,51 @@
+# remembering objects
+
+class Song:
+
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        #store the entire instance inside the all attribut
+        Song.all.append(self)
+        #returns a printable version o the class instance
+    def __repr__(self):
+        return f"(Song <Name:{self.name}>)"
+    
+
+
+Bodak_Yellow = Song("Bodak Yellow")
+song2 = Song("Brother Stone")
+print(Song.all)
+
+
+
+#Example 2
+class ShoppingList:
+    all = []
+    total = 0
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        #store the instance inside the all attribute
+        ShoppingList.all.append(self)
+
+    def __repr__(self):
+        return f"<ItemName:{self.name}, Price: {self.price}, Quantity: {self.quantity}>"
+
+    @classmethod
+    def calculate_total(cls):
+        total = 0
+        for n in cls.all:
+            total += n.price*0.9 * n.quantity
+        return total
+
+
+
+item1 = ShoppingList(name = "Sugar", price=140, quantity=1)
+item2 = ShoppingList(quantity=2, name="Colgate", price=200)
+item3 = ShoppingList("Majani Chai", 300,1)
+item4 = ShoppingList("Mouse", 500, 2)
+print(ShoppingList.all)
+print("Total", ShoppingList.calculate_total())
